4966c2961b2677d144228c6576f471366c700f18
fonfano <fonfano@gmail.com>
2023-03-16 16:13:49 +0100

    Update DeezerEnhancer.user.js

--- DeezerEnhancer.user.js
+++ DeezerEnhancer.user.js
@@ -3,7 +3,7 @@
 // @namespace   github.com/fonfano
 // @match       https://www.deezer.com/fr/*
 // @grant       none
-// @version     0.5.1
+// @version     0.5.2
 // @author      Lt Ripley
 // @description Ajoute une image (album/artiste/avatar perso) et du texte en bas a gauche et augmente la taille de la seekbar en bas
 // ==/UserScript==
@@ -11,7 +11,8 @@
 
 
 // Historique
-//  03/06/2022  Correction  v 0.5.1 Fonctionne avec les nouveau noms d'éléments
+//  16/03/2023  Correction  v 0.5.2 Adaptation au nouveau nom d'un élément
+//  03/06/2022  Correction  v 0.5.1 Fonctionne avec les nouveaux noms d'éléments
 //  26/02/2022  Upgrade     v 0.5   Ajout des numéros de titres dans les coups de coeur (fonction deezer qui a disparu)
 //  26/08/2021  Upgrade     v 0.4.1 Ajout texte : pseudo perso ou nom artiste dans page d'artiste
 //  25/08/2021  Upgrade     v 0.4   Ajout texte : nom artist et album
@@ -43,12 +44,12 @@ var albumTextDiv = document.createElement("div" );
 
 
 function numberTheTitles() {
-  
+
   if (displayTitleNumber && window.location.href.indexOf("loved") > -1 )  {
-  
+
     //let titres = document.querySelectorAll(".JoTQr"); // OLD
     let titres = document.querySelectorAll("._2OACy");
-    
+
     console.log(titres.length);
 
     console.log('numberTheTitles est appelé')
@@ -79,9 +80,9 @@ function numberTheTitles() {
       titre.querySelector("._1caJL").appendChild(maDiv);
 
     }
-    
+
   }
-  
+
 }
 
 
@@ -92,7 +93,7 @@ function numberTheTitles() {
 window.onload = (event) => { // quand la page est chargée (mais faut quand même un setTimeout)
 
   console.log('window.onload passe');
-  
+
   double(); // en dehors du timeout
 
   setTimeout(() => {
@@ -100,11 +101,11 @@ window.onload = (event) => { // quand la page est chargée (mais faut quand mêm
       // Partie seekbar (barre de progression en bas dans le morceau en cours)
       seekbar = document.querySelector("#page_player > div > div.player-track > div > div.track-seekbar > div > div.slider-track");
       seekbar.style.minHeight = seekbarHeight;
-      
+
       // ajout du numero de titre
       numberTheTitles();
-      
-    
+
+
   }, delay);
 
 };
@@ -122,7 +123,7 @@ window.addEventListener('load', (event) => { // autre façon que window.onload c
   setTimeout(() => {
 
     let elementToObserve2 = document.querySelector("#page_profile");
-    
+
     let observer2 = new MutationObserver(numberTheTitles); // déclaration avec appel fonction
 
     let options2 = {
@@ -175,18 +176,18 @@ window.addEventListener('load', (event) => { // autre façon que window.onload c
 function double() {   // Appelée au 1er chargement et aussi par l'observer quand un changement est détecté.
                       // Lance 2 fois cloneArt() : tenter une fois 2sec après le chargement de page puis une nouvelle fois à 4s par sécurité car 2s est parfois trop court
   setTimeout(() => {
-  
+
     cloneArt();
-      
+
   }, (delay/2));
-             
-             
+
+
   setTimeout(() => {
-  
+
     cloneArt();
-      
+
   }, delay);
-             
+
 }
 
 
@@ -194,12 +195,14 @@ function double() {   // Appelée au 1er chargement et aussi par l'observer quan
 function cloneArt() { // appelé par double().  Fait le boulot (image) et lance text()
 
   console.log('cloneArt est appelé');
-  
+
   let artFound = false;
 
-  leftBar = document.querySelector("#page_sidebar > div.sidebar-nav.nano.has-scrollbar > div.nano-content > div");
 
-  try { 
+  leftBar = document.querySelector("#page_sidebar");
+  //leftBar = document.querySelector("#page_sidebar > div.sidebar-nav.nano.has-scrollbar > div.nano-content > div");
+
+  try {
     leftBar.removeChild(artClone);
   }
   catch (error) {
@@ -232,29 +235,29 @@ function cloneArt() { // appelé par double().  Fait le boulot (image) et lance 
     leftBar.appendChild(artClone);
 
   }
-    
-    
+
+
   addText();  // partie texte en dessous de l'image
                 // je le mets ici sinon pour le mettre dans double() faudrait un timeout car il arrive avant l'image
-  
+
 }
 
 
 
 function addText() {  // ajouter sous l'image nom artiste et album ou nom artiste ou pseudo perso
-  
-  try { 
+
+  try {
     albumTextDiv.innerHTML="";
     artistTextDiv.innerHTML="";
   }
   catch (error) {
     console.error(error);
   }
-  
-  
+
+
   // Artiste + album
-  if (document.querySelector("#page_naboo_album > div:nth-child(1) > div > div._5BJsj > div > div._2Rv6Z._2j25R"))  {  
-    
+  if (document.querySelector("#page_naboo_album > div:nth-child(1) > div > div._5BJsj > div > div._2Rv6Z._2j25R"))  {
+
     let artist = document.querySelector("#page_naboo_album > div:nth-child(1) > div > div._5BJsj > div > div._2yyo6 > div > h2").innerText;
     let album = document.querySelector("#page_naboo_album > div:nth-child(1) > div > div._5BJsj > div > div._2yyo6 > h1").innerText;
 
@@ -268,7 +271,7 @@ function addText() {  // ajouter sous l'image nom artiste et album ou nom artist
     artistTextDiv.style.marginLeft = "10px";
     artistTextDiv.style.marginRight = "10px";
     artistTextDiv.style.textAlign = "left";
-    
+
     albumTextDiv.style.fontSize = "15px";
     albumTextDiv.style.marginLeft = "10px";
     albumTextDiv.style.marginRight = "10px";
@@ -279,29 +282,29 @@ function addText() {  // ajouter sous l'image nom artiste et album ou nom artist
     leftBar.appendChild(textDiv);
 
   }
-  
-  
+
+
   // Artiste seul
   if (document.querySelector("#page_naboo_artist > div._5BJsj.container > div > div._2Rv6Z._2j25R._2w27N._2L8AT"))  {
-  
+
     let artist = document.querySelector("#page_naboo_artist > div._5BJsj.container > div > div._2yyo6._3Ppj- > h1").innerText;
-    
+
     artistTextDiv.appendChild(document.createTextNode(artist));
     artistTextDiv.style.fontSize = "20px";
     artistTextDiv.style.marginLeft = "10px";
     artistTextDiv.style.marginRight = "10px";
     artistTextDiv.style.textAlign = "center";
-    
+
     textDiv.appendChild(artistTextDiv);
-    
+
     leftBar.appendChild(textDiv);
-      
+
   }
-  
-  
+
+
   // Pseudo perso
   if (document.querySelector("#page_profile > div.profile-header-container > div > div > div > div._2Rv6Z._2j25R._2w27N._2L8AT"))  {
-    
+
     let pseudo = document.querySelector("#page_profile > div.profile-header-container > div > div > div > div._2yyo6.profile-header-info > h1").innerText;
     // je mets le pseudo dans le div artiste pour pas me faire chier
     artistTextDiv.appendChild(document.createTextNode(pseudo));
@@ -309,14 +312,14 @@ function addText() {  // ajouter sous l'image nom artiste et album ou nom artist
     artistTextDiv.style.marginLeft = "10px";
     artistTextDiv.style.marginRight = "10px";
     artistTextDiv.style.textAlign = "center";
-    
+
     textDiv.appendChild(artistTextDiv);
-    
+
     leftBar.appendChild(textDiv);
-    
-    
+
+
   }
-    
-  
+
+
 }
 
